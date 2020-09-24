f0e7480dd3ac70eab81c5cd0a3f81456afb18be6
hermann225-zrouama <62513048+hermann225-zrouama@users.noreply.github.com>
2020-09-24 22:35:04 +0000

    Create day_two.py

---
+++ day_two.py
@@ -0,0 +1,41 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Uber.
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+"""
+
+
+def day_two(array):
+    return [multiplication(array)/n for n in (array)]
+
+
+def bonus(array):
+    second_array = []
+    save = array.copy()
+    new_array = []
+    for i in range(len(array)):
+        array.pop(i)
+        second_array = array.copy()
+        new_array.append(multiplication(second_array))
+        array = save.copy()
+    return new_array
+
+
+def multiplication(array):
+    res = 1
+    for i in range(len(array)):
+        res = res*array[i]
+    return res
+
+
+if __name__ == "__main__":
+    print(day_two([1, 2, 3, 4, 5]))
+    print(bonus([1, 2, 3, 4, 5]))
+    print(day_two([3, 2, 1]))
+    print(bonus([3, 2, 1]))
